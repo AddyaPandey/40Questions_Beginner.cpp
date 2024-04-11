c2df1a5be93603e1283082a014dac5a84c75a045
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-04-12 00:29:40 +0530

    j

---
+++ c++/sumFrom1ToN.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+// sum from 1 to 5
+
+int n;
+cin>>n;
+int i = 1;
+int sum = 0;
+
+while(i<=n){
+sum = sum + i; //
+i= i + 1; //2
+
+}
+cout<<" sum of all numbers is :"<<sum;
+
+}
