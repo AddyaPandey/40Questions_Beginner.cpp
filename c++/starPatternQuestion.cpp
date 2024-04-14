23b7eacf9bb39e674c771d146e90c3376091024b
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-04-14 19:37:40 +0530

    j

---
+++ c++/starPatternQuestion.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+                                // while loop
+int n;
+cin>>n;                 //print star star star 
+
+int row=1;
+while(row<=n){            //pattern questions
+
+int column=1;
+while(column<=n) {
+
+    cout<<"*";
+    column= column+1;
+                }
+    cout<<endl;
+row=row+1;
+           }
+         }     
