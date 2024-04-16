de7a805dce3bf39c461f0aa2e57872597b3c98bf
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-04-16 23:50:35 +0530

    k

---
+++ c++/nestedLoopQuestion.cpp
@@ -0,0 +1,32 @@
+// print this pattern
+//12345
+//1234
+//123
+//12
+//1
+//12
+//123
+//1234
+//12345
+
+#include<iostream>
+using namespace std;
+int main(){
+
+int n;
+cin>>n;
+
+int row=1;
+while(row<=n) {
+
+int column=1;
+while(column<=n) {
+
+cout<<column;
+column=column+1;
+
+}
+cout<<endl;
+row=row+1;
+}
+} 
