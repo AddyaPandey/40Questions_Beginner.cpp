6d8f60ca19c9b51f1259b6d436f63aacdef9e185
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-06-24 19:53:59 +0530

    01

---
+++ c++/30DaysOfCode.cpp/day1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+cout<< "Hello, World."<<endl;
+
+string inputString= "Welcome to 30 days of code!";
+cout << inputString << endl;
+
+    return 0;
+}
---
+++ c++/oops.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher {
+
+// properties/ attributes
+string name;
+string department;
+string subject;
+double salary;
+
+//methods /member functions 
+void changeDepartment(string newDepartment) {
+     department = newDepartment;
+                                            }
+};
+
+int main() {
+ Teacher
+
+return 0;
+}
