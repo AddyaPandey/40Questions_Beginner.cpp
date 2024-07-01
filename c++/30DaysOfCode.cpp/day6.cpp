39b88481ca6c914f61020d6d419d6efa75eab1fb
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-07-01 23:52:11 +0530

    06

---
+++ c++/30DaysOfCode.cpp/day6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+int n;
+cin>>n;
+
+if (n>5) {
+ cout<<"greater than 5"<<endl;
+}
+else if (n<5) {
+ cout<<"Less than 5"<<endl;
+}
+else if (n=5) {
+ cout<<"Equal to 5";
+}
+
+    return 0;
+}
