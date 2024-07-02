614c92f685bb36e3ab7a6f3e39ee2cc9fc9e662e
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-07-02 23:58:35 +0530

    08

--- c++/30DaysOfCode.cpp/day7.cpp
+++ c++/30DaysOfCode.cpp/day7.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 using namespace std;
-
-
 int main() {
-int n;
-cin>>n;
-
-if (n>5) {
- cout<<"greater than 5"<<endl;
+int a=1;
+switch(a){
+    case 1: cout<<"one";
+    case 2: cout<<"two";
+    case 3: cout<<"three";
+    case 4: cout<<"four";
+    case 5: cout<<"five";
+    case 6: cout<<"six";
+    case default: cout<<"default";
 }
-else if (n<5) {
- cout<<"Less than 5"<<endl;
-}
-else if (n=5) {
- cout<<"Equal to 5";
-}
-
     return 0;
 }
---
+++ c++/day8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int j=0;
+
+while(j<3){
+
+for (int i=0; i<5, i++;) {
+    cout<<i<<endl;
+
+   if (i==2) {
+    cout<<"done"<<endl;
+    break;
+   }
+   }
